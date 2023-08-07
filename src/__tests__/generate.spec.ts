7245efb7ded3f3ee275254408eb9653ed28e2657
rohit1901 <rohit1901@users.noreply.github.com>
2023-08-07 14:05:59 +0000

    Prettified Code!

--- src/__tests__/generate.spec.ts
+++ src/__tests__/generate.spec.ts
@@ -1,32 +1,39 @@
-import {capitalize, isEnumString, isInterfaceString, isObject, isTypeString, removeWhitespace,} from '../utils';
+import {
+  capitalize,
+  isEnumString,
+  isInterfaceString,
+  isObject,
+  isTypeString,
+  removeWhitespace,
+} from '../utils';
 
 describe('capitalize', () => {
-    it('should capitalize a string', () => {
-        const result = capitalize('hello');
-        expect(result).toEqual('Hello');
-    });
-    it('should capitalize a string with multiple words', () => {
-        const result = capitalize('hello world');
-        expect(result).toEqual('Hello world');
-    });
-    it('should test Enum String', () => {
-        const result = isEnumString('hello world');
-        expect(result).toEqual(false);
-    });
-    it('should test Interface String', () => {
-        const result = isInterfaceString('hello world');
-        expect(result).toEqual(false);
-    });
-    it('should test Type String', () => {
-        const result = isTypeString('hello world');
-        expect(result).toEqual(false);
-    });
-    it('should test Object', () => {
-        const result = isObject('hello world');
-        expect(result).toEqual(false);
-    });
-    it('should test removeWhitespace', () => {
-        const result = removeWhitespace('hello world');
-        expect(result).toEqual('helloworld');
-    });
+  it('should capitalize a string', () => {
+    const result = capitalize('hello');
+    expect(result).toEqual('Hello');
+  });
+  it('should capitalize a string with multiple words', () => {
+    const result = capitalize('hello world');
+    expect(result).toEqual('Hello world');
+  });
+  it('should test Enum String', () => {
+    const result = isEnumString('hello world');
+    expect(result).toEqual(false);
+  });
+  it('should test Interface String', () => {
+    const result = isInterfaceString('hello world');
+    expect(result).toEqual(false);
+  });
+  it('should test Type String', () => {
+    const result = isTypeString('hello world');
+    expect(result).toEqual(false);
+  });
+  it('should test Object', () => {
+    const result = isObject('hello world');
+    expect(result).toEqual(false);
+  });
+  it('should test removeWhitespace', () => {
+    const result = removeWhitespace('hello world');
+    expect(result).toEqual('helloworld');
+  });
 });
