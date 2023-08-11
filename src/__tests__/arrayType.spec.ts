853ed3ccbb2694c034dc6fecac804f0555b63e6b
rohit1901 <rohit1901@users.noreply.github.com>
2023-08-11 14:10:47 +0000

    Prettified Code!

--- src/__tests__/arrayType.spec.ts
+++ src/__tests__/arrayType.spec.ts
@@ -1,21 +1,22 @@
-import {createSourceFile} from "typescript";
-import * as ts from "typescript";
-import {generateInterfaceTypeGuard} from "../api";
-import {removeWhitespace} from "../utils";
+import { createSourceFile } from 'typescript';
+import * as ts from 'typescript';
+import { generateInterfaceTypeGuard } from '../api';
+import { removeWhitespace } from '../utils';
 
 describe('Enum types', () => {
-    const text = `interface ArrayInterface {
+  const text = `interface ArrayInterface {
       arrayProperty: string[];
       arrayOptional?: string[];
     }`;
-    const sourceFile = createSourceFile('', text, ts.ScriptTarget.ES2015, true);
-    const interfaces = sourceFile.statements.filter(ts.isInterfaceDeclaration);
-    beforeEach(() => {
-        jest.clearAllMocks();
-    });
-    it('should correctly generate typeguards for required and optional properties (array of strings) of an interface', () => {
-        const result = generateInterfaceTypeGuard(interfaces);
-        const expectedResult = removeWhitespace(`export function isArrayInterface(value: any): value is ArrayInterface {
+  const sourceFile = createSourceFile('', text, ts.ScriptTarget.ES2015, true);
+  const interfaces = sourceFile.statements.filter(ts.isInterfaceDeclaration);
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  it('should correctly generate typeguards for required and optional properties (array of strings) of an interface', () => {
+    const result = generateInterfaceTypeGuard(interfaces);
+    const expectedResult =
+      removeWhitespace(`export function isArrayInterface(value: any): value is ArrayInterface {
           return (
             typeof value === "object" &&
             value !== null &&
@@ -26,7 +27,7 @@ describe('Enum types', () => {
               (Array.isArray(value.arrayOptional) &&
                 value.arrayOptional.every((item: any) => typeof item === 'string')))
           )
-        }`)
-        expect(removeWhitespace(result)).toEqual(expectedResult);
-    });
+        }`);
+    expect(removeWhitespace(result)).toEqual(expectedResult);
+  });
 });
