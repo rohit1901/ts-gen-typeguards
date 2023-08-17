dbb8b0e5def7f276246d618de71dab9bb0791b5c
rohit1901 <rohit1901@users.noreply.github.com>
2023-08-17 21:04:04 +0000

    Prettified Code!

--- src/__tests__/generics.spec.ts
+++ src/__tests__/generics.spec.ts
@@ -1,24 +1,24 @@
-import {createSourceFile} from "typescript";
-import * as ts from "typescript";
-import {generateEnumTypeGuards, generateInterfaceTypeGuard} from "../api";
-import {removeWhitespace} from "../utils";
+import { createSourceFile } from 'typescript';
+import * as ts from 'typescript';
+import { generateEnumTypeGuards, generateInterfaceTypeGuard } from '../api';
+import { removeWhitespace } from '../utils';
 
 describe('Generator', () => {
-    const text = `export interface GenericInterface<T> {
+  const text = `export interface GenericInterface<T> {
         property: T;
         nestedProp: {
             nested: T
         }
     }`;
-    const sourceFile = createSourceFile('', text, ts.ScriptTarget.ES2015, true);
-    const interfaces = sourceFile.statements.filter(ts.isInterfaceDeclaration);
-    beforeEach(() => {
-        jest.clearAllMocks();
-    });
-    it('should generate typeguard for an Interface with a generic property and a nested generic property', function () {
-        const result = generateInterfaceTypeGuard(interfaces);
-        expect(removeWhitespace(result)).toEqual(
-            removeWhitespace(`
+  const sourceFile = createSourceFile('', text, ts.ScriptTarget.ES2015, true);
+  const interfaces = sourceFile.statements.filter(ts.isInterfaceDeclaration);
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  it('should generate typeguard for an Interface with a generic property and a nested generic property', function () {
+    const result = generateInterfaceTypeGuard(interfaces);
+    expect(removeWhitespace(result)).toEqual(
+      removeWhitespace(`
         export function isGenericInterface<T>(
           val: any,
           guard: (val: any) => val is T
@@ -33,6 +33,6 @@ describe('Generator', () => {
             guard(value.nestedProp.nested))
           )
         }`),
-        );
-    });
+    );
+  });
 });
